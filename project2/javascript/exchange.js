d7867a958d3a3f05df475cd96d27cf55cacd4b7a
Jack Hoffman <32367528+Jacker71300@users.noreply.github.com>
2019-12-04 13:03:57 -0500

    Did some stuff

--- project2/javascript/exchange.js
+++ project2/javascript/exchange.js
@@ -1,11 +1,24 @@
 "use strict";
+let conversionTable;
+let output;
+let base;
+let conversion;
+let amount;
 window.onload = getData;
 
 function getData(){
+    base = document.querySelector("#base");
+    conversion = document.querySelector("#conversion");
+    amount = document.querySelector("#amount");
+    output = document.querySelector("#output");
+    let submit = document.querySelector("#submit");
+
+    submit.addEventListener("click", updateValue);
+
+
     let xhr = new XMLHttpRequest();
     let url = "https://api.exchangeratesapi.io/latest/?base=USD";
 
-    
     xhr.open("GET", url);
  
     xhr.send(null);
@@ -16,10 +29,36 @@ function getData(){
 function dataLoaded(e){
     let xhr = e.target;
 
-    console.log(xhr.responseText);
-    document.querySelector("#response").innerHTML = xhr.responseText;
+    let response = JSON.parse(xhr.responseText);
+
+    conversionTable = response.rates;
+
+    for (let key in conversionTable){
+        let option = document.createElement("option");
+        option.value = key;
+        option.innerHTML = key;
+        let value = option.cloneNode(true);
+        base.appendChild(option);
+        conversion.appendChild(value);
+    }
+
+    console.log("test");
 }
 
 function dataError(){
     console.log("An error occurred");
+}
+
+function updateValue(){
+    console.log(amount.value);
+    let num = parseInt(amount.value);
+    console.log(num);
+    let exchangeRate = conversionTable[base.value] / conversionTable[conversion.value];
+    console.log(exchangeRate);
+
+    if(num){
+        let value = num * exchangeRate;
+        console.log(value);
+        output.innerHTML = value.toFixed(2);
+    }
 }
